fcdb478f26e7e6042e43f65bca4ced560d1284c6
ronaldding <ronaldding2002@gmail.com>
2025-07-29 00:16:45 +0800

    modified:   src/react-app/NotFoundPage.tsx

--- src/react-app/NotFoundPage.tsx
+++ src/react-app/NotFoundPage.tsx
@@ -1,9 +1,3 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
-import honoLogo from "./assets/hono.svg";
 import "./NotFoundPage.css";
 
 const NotFoundPage = () => {
